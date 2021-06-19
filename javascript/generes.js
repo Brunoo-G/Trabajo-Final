6e8f290fe5946f1d95725d5ccbe9fd4d28a47777
Bruno Gonzalez <bruno_nicolas88@hotmail.com>
2021-06-19 00:11:51 -0300

    Empezando con local storage

--- javascript/generes.js
+++ javascript/generes.js
@@ -2,6 +2,8 @@ window.addEventListener("load", function(){
 
     const proxy = 'https://cors-anywhere.herokuapp.com/';
 
+    /* Lista de generos */ 
+
     fetch(`${proxy}https://api.deezer.com/genre`)
         .then(function (response){
             return response.json();
@@ -62,15 +64,29 @@ window.addEventListener("load", function(){
             console.log("el error es:" + error)
         });
 
-    fetch(`${proxy}`)
-        .then(function (response){
-            return response.json();
-        })
-        .then (function(datos){
-            console.log(datos);
-            document.querySelector('#genero_6').innerHTML = `<a href="detail-genres.html"><h2>${datos.data[1].name}</h2></a>`
-        })
-        .catch(function(error){
-            console.log("el error es:" + error)
-        });
-})
+
+    /* Eventos de cada genero */ 
+
+    // document.querySelector('#genero_1').addEventListener('click' , function(){
+    //     localStorage.setItem('rap', JSON.stringify(`${datos.data[2].name}`));
+    // });
+
+    // document.querySelector('#genero_2').addEventListener('click' , function(){
+    //     localStorage.setItem('rap', JSON.stringify(`${datos.data[3].name}`));
+    // });
+    
+    // document.querySelector('#genero_3').addEventListener('click' , function(){
+    //     localStorage.setItem('rap', JSON.stringify(`${datos.data[19].name}`));
+    // });
+
+    // document.querySelector('#genero_4').addEventListener('click' , function(){
+    //     localStorage.setItem('rap', JSON.stringify(`${datos.data[5].name}`));
+    // });
+
+    // document.querySelector('#genero_5').addEventListener('click' , function(e){
+    //     e.preventDefault();
+    //     localStorage.setItem('rap', JSON.stringify(`${datos.data[1].name}`));
+    // });
+
+});
+
